ebcb1b2b19515b300ad6916ba1062f05f48d08da
Arthur <ar.roschal@gmail.com>
2026-04-14 22:59:51 -0400

    Telegram Bot Revised

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#from background import keep_alive
-#from bot.handlers import bot
+from background import keep_alive
+from bot.handlers import bot
 
-#if __name__ == "__main__":
-    #keep_alive()
-    #bot.polling(non_stop=True, interval=1)
+if __name__ == "__main__":
+    keep_alive()
+    bot.polling(non_stop=True, interval=1)
+
